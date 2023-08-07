89ade4671cace623ef00ab501ce56958ff60b1e2
Lei Gao <114420297+TomLei99@users.noreply.github.com>
2023-08-07 09:13:15 +0100

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -929,6 +929,8 @@ async function getMyAuction() {
                                                                                                 status.textContent = 'Status: Ended';
                                                                                             } else if (started) {
                                                                                                 status.textContent = 'Status: In process';
+                                                                                            } else if (started && endAt < Date.now() / 1000) {
+                                                                                                status.textContent = 'Wait for seller to end';
                                                                                             } else {
                                                                                                 status.textContent = 'Status: Not Start';
                                                                                             }
